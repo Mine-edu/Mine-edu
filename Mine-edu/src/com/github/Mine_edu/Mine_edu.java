ab049fdf4de7753b5be04651e8586b868b03be63
Mine-edu <mined.edu@gmail.com>
2012-04-10 17:35:37 +0800

    with source

---
+++ Mine-edu/src/com/github/Mine_edu/Mine_edu.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.github.Mine_edu;
+
+/**
+ * @author root
+ *
+ */
+public class Mine_edu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
